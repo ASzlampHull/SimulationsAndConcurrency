fc0e882b42868dbb83f6f7d8a21bdfc623b12f76
ASzlampHull <A.SZLAMP-2023@hull.ac.uk>
2026-02-17 09:44:08 +0000

    Added the glm library

--- Exercises/SimulationStaticLib/Sphere.h
+++ Exercises/SimulationStaticLib/Sphere.h
@@ -1,11 +1,17 @@
 #pragma once
+#include "glm.hpp"
+#include "gtc/matrix_transform.hpp"
+
 
 class Sphere {
 private:
 	double radius = 10.0;
+	glm::vec3 center = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::vec3 velocity = glm::vec3(0.0f, 0.0f, 0.0f);
 
 public:
-	Sphere(double radius_) : radius(radius_) {}
+	Sphere(double radius_, glm::vec3 center_, glm::vec3 velocity_)
+		: radius(radius_), center(center_), velocity(velocity_) {}
 	~Sphere() = default;
 
 	double getRadius() const;
